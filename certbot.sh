4042a1d65f92df6e6bc5d5289e4cbf6cdb524bcb
Fernando Muniz <fernandomxm@gmail.com>
2026-05-15 15:27:31 -0300

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo certbot --manual --preferred-challenges dns certonly --manual-auth-hook /home/fernando/certificados/gcp.sh -d *.dominio.com.br
+
+## Manual
+sudo certbot --manual --preferred-challenges dns certonly --email fernando@gmail.com --agree-tos --eff-email -d *.dominio.com.br
